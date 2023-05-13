5179fd72de9e1b48fda987c293eb4275126f3e56
Emre <degirmenci.a.emre@gmail.com>
2023-05-13 11:25:25 +0300

    Initial Commit

---
+++ Tests/EDCarouselSPMTests/EDCarouselSPMTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import EDCarouselSPM
+
+final class EDCarouselSPMTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(EDCarouselSPM().text, "Hello, World!")
+    }
+}
